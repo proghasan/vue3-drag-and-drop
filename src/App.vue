88a347752fc86e90e17af15f3c9dd3c0e3a77ff4
proghasan <mohammaddothasan@gmail.com>
2022-07-22 01:02:39 +0600

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,100 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div 
+    class="drag-donze"
+    @drop="onDrop($event, 1)"
+    @dragenter.prevent
+    @dragover.prevent
+  >
+    <div 
+      v-for="item in getList(1)" 
+      :key="item.id" 
+      class="drag-el" 
+      draggable="true"
+      @dragstart="startDrag($event, item)"
+    >
+      {{ item.title }}
+    </div>
+  </div>
+  <div 
+    class="drag-donze"
+    @drop="onDrop($event, 2)"
+    @dragenter.prevent
+    @dragover.prevent
+  >
+    <div 
+      v-for="item in getList(2)" 
+      :key="item.id" 
+      class="drag-el" 
+      draggable="true"
+      @dragstart="startDrag($event, item)"
+    >
+      {{ item.title }}
+    </div>
+  </div>
 </template>
+<script>
+import { ref } from 'vue'
 
-<style>
+export default {
+  setup() {
+    const items = ref([
+      { id: 0, title: 'Category A', list: 1 },
+      { id: 1, title: 'Category B', list: 1 },
+      { id: 2, title: 'Category C', list: 2 },
+    ])
+
+    const getList = (list) => {
+      return items.value.filter(item => item.list == list)
+    }
+
+    const startDrag = (event, item) => {
+      console.log(item)
+      event.dataTransfer.dropEffect = 'move'
+      event.dataTransfer.effectAllowed = 'move'
+      event.dataTransfer.setData('itemID', item.id)
+    }
+
+    const onDrop = (event, list) => {
+      const itemID = event.dataTransfer.getData('itemID')
+      const item = items.value.find(item => item.id == itemID)
+      item.list = list
+    }
+    
+    return {
+      getList,
+      startDrag,
+      onDrop
+    }
+  }
+}
+</script>
+
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
 
-nav {
-  padding: 30px;
+.drag-donze {
+  width: 50%;
+  margin: 50px auto;
+  background-color: #abbdcf;
+  padding: 10px;
+  min-height: 10px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.drag-el {
+  background-color: rgb(58, 58, 131);
+  color: white;
+  padding: 5px;
+  margin-bottom: 10px;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.drag-el:nth-last-of-type(1) {
+  margin-bottom: 0;
 }
 </style>
